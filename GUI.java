b674a3bff216f46ecb6650a8be0347ae347708cf
sniperkattenSkol <te15.nils.lindfors@umea.nti.se>
2018-06-05 10:26:16 +0200

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -1,48 +1,75 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
 // Nils E Lindfors TE15
 // 2018-01-31
 // 13:03
+
+/**
+ * Main class
+ */
 public class GUI implements ActionListener {
+    /**
+     *  x , is the width
+     *  y , is the height
+     *  ColorTurn, is whoes turn it is, 1 is red and 2 is yellow.
+     *  No , is a space on the board,
+     *  rowNum , is the numb of rows on
+     *  ButtonXY , is the dimension
+     */
     private static int x = 595;
     private static int y = 700;
     public static  int ColorTurn = 1;
     public static int nO[][] = new int[10][10]; //42 [7][6]
     public static int rowNum[] = new int[7];
     private static Dimension ButtonXY = new Dimension(68,100);
-    public static void createGUI(){
+
+    /**
+     * Creates the GUI
+     * @throws IOException
+     * Creates a JFrame called mainFrame, adds the dimensions to it and sets a few options related to it.
+     * Sets the background to blue.
+     */
+    public static void createGUI() throws IOException {
         JFrame mainFrame = new JFrame("Fyra i rad");
-        mainFrame.setPreferredSize(new Dimension(x,y));
+        mainFrame.setPreferredSize(new Dimension(x, y));
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setResizable(false);
-        //BufferedImage board = new BufferedImage(595,700,BufferedImage.TYPE_3BYTE_BGR);
         mainFrame.setBackground(Color.blue);
-        //Remake this with "dynmaic paint" google this
-        mainFrame.setContentPane(new JPanel(){
+        mainFrame.setContentPane(new JPanel() {
             //Paints the game board
-            public void  paintComponent(Graphics g){
+            public void paintComponent(Graphics g) {
+                /**
+                 * Creates the board that is to be filled with game pawns.
+                 */
                 g.setColor(Color.WHITE);
-               // g.drawImage(board,0,0,null);
                 int rowCords = 165;
                 int colCords = 0;
-                for (int row = 1;row < 7; row++){
-                    for (int col = 1; col < 8; col++){
-                        g.fillOval(colCords,rowCords,80,80);
-                        colCords+= 85;
+                for (int row = 1; row < 7; row++) {
+                    for (int col = 1; col < 8; col++) {
+                        g.fillOval(colCords, rowCords, 80, 80);
+                        colCords += 85;
                     }
                     colCords = 0;
-                    rowCords+= 85;
+                    rowCords += 85;
                 }
             }
         });
 
-
-
-        JButton [] ArrowButtons = new JButton[8];
-        //Creates 8 buttons
-        for (int l = 0; l < 7; l++){
+        /**
+         * @param Winner , image loaded from web to be displayed for the winner.
+         * @param ArrowButtons, creates 8 buttons.
+         * Creates and sets the looks of the buttons in a for loop.
+         */
+        BufferedImage Winner = ImageIO.read(new URL("https://i.imgur.com/7T3sCHq.gif"));
+        JButton[] ArrowButtons = new JButton[8];
+        for (int l = 0; l < 7; l++) {
             ArrowButtons[l] = new JButton("V");
             ArrowButtons[l].setBackground(Color.black);
             ArrowButtons[l].setBorderPainted(false);
@@ -52,46 +79,45 @@ public class GUI implements ActionListener {
             ArrowButtons[l].setFont(new Font("Arial", Font.PLAIN, 50));
             mainFrame.add(ArrowButtons[l]);
 
+            //06 12 18 24 30 36 42
+            //05 11 17 23 29 35 41
+            //04 10 16 22 28 34 40
+            //03 09 15 21 27 33 39
+            //02 08 14 20 26 32 38
+            //01 07 13 19 25 31 37
+            //visualization of oval layout in oval ARRAY
+            // 0 is white, 1 is red, 2 is yellow
+            /**
+             * Chceks if the desierd button is pressed. And if it checks if its yellow or reds turn.
+             * Then it adds the pawn to the game board and increments that specific rowNum.
+             * If the row is filled it cant be filled and the turn dosent register.
+             */
             ArrowButtons[l].addActionListener(new ActionListener() {
-
-                //06 12 18 24 30 36 42
-                //05 11 17 23 29 35 41
-                //04 10 16 22 28 34 40
-                //03 09 15 21 27 33 39
-                //02 08 14 20 26 32 38
-                //01 07 13 19 25 31 37
-                //visualization of oval layout in oval ARRAY
-                // 0 is white, 1 is red, 2 is yellow
-                //
-
-                //191 is the top oval
                 @Override
-                public void  actionPerformed(ActionEvent e) {
+                public void actionPerformed(ActionEvent e) {
                     if (e.getSource() == ArrowButtons[0]) {
-                         System.out.println("test 1");
-                             int tempnumb = rowNum[0];
-                             tempnumb = 616 - tempnumb * 85;
-                             if (rowNum[0] < 6) {
-                                 if (ColorTurn == 1) {
-                                     Graphics g = mainFrame.getGraphics();
-                                     g.setColor(Color.RED);
-                                     g.fillOval(3, tempnumb, 81, 81);
-                                     ColorTurn = 2;
-                                     nO[0][rowNum[0]] = 1;
-                                     rowNum[0]++;
-                                 } else if (ColorTurn == 2) {
-                                     Graphics g = mainFrame.getGraphics();
-                                     g.setColor(Color.YELLOW);
-                                     g.fillOval(3, tempnumb, 81, 81);
-                                     ColorTurn = 1;
-                                     nO[0][rowNum[0]] = 2;
-                                     rowNum[0]++;
-                                 }
-                             }
+                        int tempnumb = rowNum[0];
+                        tempnumb = 616 - tempnumb * 85;
+                        if (rowNum[0] < 6) {
+                            if (ColorTurn == 1) {
+                                Graphics g = mainFrame.getGraphics();
+                                g.setColor(Color.RED);
+                                g.fillOval(3, tempnumb, 81, 81);
+                                ColorTurn = 2;
+                                nO[0][rowNum[0]] = 1;
+                                rowNum[0]++;
+                            } else if (ColorTurn == 2) {
+                                Graphics g = mainFrame.getGraphics();
+                                g.setColor(Color.YELLOW);
+                                g.fillOval(3, tempnumb, 81, 81);
+                                ColorTurn = 1;
+                                nO[0][rowNum[0]] = 2;
+                                rowNum[0]++;
+                            }
+                        }
 
                     } else if (e.getSource() == ArrowButtons[1]) {
 
-                        System.out.println("test 2");
                         int tempnumb = rowNum[1];
                         tempnumb = 616 - tempnumb * 85;
                         if (rowNum[1] < 6) {
@@ -112,8 +138,7 @@ public class GUI implements ActionListener {
                             }
                         }
 
-                    }else if (e.getSource() == ArrowButtons[2]) {
-                        System.out.println("test 3");
+                    } else if (e.getSource() == ArrowButtons[2]) {
                         int tempnumb = rowNum[2];
                         tempnumb = 616 - tempnumb * 85;
                         if (rowNum[2] < 6) {
@@ -134,8 +159,7 @@ public class GUI implements ActionListener {
                             }
                         }
 
-                    }else if (e.getSource() == ArrowButtons[3]) {
-                        System.out.println("test 4");
+                    } else if (e.getSource() == ArrowButtons[3]) {
                         int tempnumb = rowNum[3];
                         tempnumb = 616 - tempnumb * 85;
                         if (rowNum[3] < 6) {
@@ -156,8 +180,7 @@ public class GUI implements ActionListener {
                             }
                         }
 
-                    }else if (e.getSource() == ArrowButtons[4]) {
-                        System.out.println("test 5");
+                    } else if (e.getSource() == ArrowButtons[4]) {
                         int tempnumb = rowNum[4];
                         tempnumb = 616 - tempnumb * 85;
                         if (rowNum[4] < 6) {
@@ -178,8 +201,7 @@ public class GUI implements ActionListener {
                             }
                         }
 
-                    }else if (e.getSource() == ArrowButtons[5]) {
-                        System.out.println("test 6");
+                    } else if (e.getSource() == ArrowButtons[5]) {
                         int tempnumb = rowNum[5];
                         tempnumb = 616 - tempnumb * 85;
                         if (rowNum[5] < 6) {
@@ -200,8 +222,7 @@ public class GUI implements ActionListener {
                             }
                         }
 
-                    }else if (e.getSource() == ArrowButtons[6]) {
-                        System.out.println("test 7");
+                    } else if (e.getSource() == ArrowButtons[6]) {
                         int tempnumb = rowNum[6];
                         tempnumb = 616 - tempnumb * 85;
                         if (rowNum[6] < 6) {
@@ -223,24 +244,27 @@ public class GUI implements ActionListener {
                         }
 
                     }
-
+                    /**
+                     * @param fyraIradVinnare, If the it returns a 1 red wins, if its a 2 yellow wins. If its a 3 its a TIE.
+                     */
                     if (fyraIradVinnare() == 1) {
-                        //röd vinner
+                        //röd wins
                         Graphics g = mainFrame.getGraphics();
-                        g.setColor(Color.RED);
-                        g.drawString("RED WINS",200,150);
+                        g.drawImage(Winner, 200, 200, null);
 
-                    }else if (fyraIradVinnare() == 2) {
-                        //Gul vinner
+                    } else if (fyraIradVinnare() == 2) {
+                        //Gul wins
                         Graphics g = mainFrame.getGraphics();
-                        g.setColor(Color.YELLOW);
-                        g.drawString("YELLOW WINS", 200, 150);
-                    } else if (fyraIradVinnare() == 3){
+                        g.drawImage(Winner, 200, 200, null);
+                        //Tie
+                    } else if (fyraIradVinnare() == 3) {
                         Graphics g = mainFrame.getGraphics();
                         g.setColor(Color.GREEN);
                         g.drawString("TIE", 200, 150);
-
-                    }else {
+                        /**
+                         * @param vemsTur, is called if no winner is found, it displays the whos turn it is by yellow or red dots under the arrows.
+                         */
+                    } else {
                         vemsTur(mainFrame);
                     }
 
@@ -250,49 +274,18 @@ public class GUI implements ActionListener {
         }
 
 
-
-
-
         mainFrame.pack();
         mainFrame.setVisible(true);
-
-
-
-
-
-
-        //mainFrame.setLayout(new GridLayout(7,8));
-
-       /* JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(7,8,2,2));
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-        JLabel newLabel = new JLabel("lll");
-
-
-        newLabel.setPreferredSize(new Dimension(98,98));
-        newLabel.setForeground(Color.RED);
-
-        /*for (int i = 0; i < 56; i++) {
-            mainFrame.add(newLabel);
-
-        }
-        */
     }
-    //The row is what oval/ovals should be painted
-    //165 per row , 85 per col
-   /* public void painterR(int ovalNumber , Graphics g){
-        int tempcol = 0;
-        if (nO[ovalNumber] == 1){
-                g.setColor(Color.red);
-                if (ovalNumber > 7){
-                    ovalNumber = ovalNumber - 7;
 
-                }
-                g.fillOval();
-            }
-    }
-*/
    //Funktionen är till så att arrayen inte går över eller under 0
+
+    /**
+     *
+     * @param b , takes in x value from nO
+     * @param c , takes in y value from nO
+     * @return returns 0 if b or c is less than 0 or greater than the board dimension, else return 0.
+     */
    public static int pjas(int b, int c){
        if (b < 0 || c < 0 || b >= 6 || c >=7){
            return 0;
@@ -301,6 +294,10 @@ public class GUI implements ActionListener {
        }
    }
 
+    /**
+     *
+     * @param mainFrame , displays red or yellow dots depending on whos turn it is.
+     */
    public static void vemsTur(JFrame mainFrame){
        if(ColorTurn == 1){
            Graphics g = mainFrame.getGraphics();
@@ -319,6 +316,10 @@ public class GUI implements ActionListener {
        }
    }
 
+    /**
+     * Checks horizontal 4inRows and if returns the No , red or yellow value.
+     * @return
+     */
     public static int fyraIradVinnare() {
         int tempnumb = 0;
         for (int q = 0; q < 6; q++) {
@@ -329,14 +330,22 @@ public class GUI implements ActionListener {
                 }
               }
             }
-            for (int q = 0; q < 6; q++) {
+        /**
+         * Checks vertical 4inRows and if returns the No, red or yellow value.
+         * @return
+         */
+        for (int q = 0; q < 6; q++) {
                 for (int w = 0; w < 7; w++) {
                     if (pjas(q,w)!=0&&pjas(q,w)==pjas(q+1,w)&&pjas(q,w)==pjas(q+2,w)&&pjas(q,w)==pjas(q+3,w)) {
                         return nO[q][w];
                     }
                 }
             }
-            for (int q = 0; q < 6; q++) {
+        /**
+         * Checks for angled 4inRows and if returns the No, red or yellow value.
+         * @return
+         */
+        for (int q = 0; q < 6; q++) {
                 for (int w = 0; w < 7; w++) {
                     for (int e = -1; e <=1; e += 2){
                         if (pjas(q,w)!=0&&pjas(q,w)==pjas(q+1*e,w+1)&&pjas(q,w)==pjas(q+2*e,w+2)&&pjas(q,w)==pjas(q+3*e,w+3)){
@@ -345,7 +354,11 @@ public class GUI implements ActionListener {
                     }
                 }
             }
-            for (int q = 0; q < 6; q++) {
+        /**
+         * Checks if there are still moves to and if returns 0.
+         * @return
+         */
+        for (int q = 0; q < 6; q++) {
                 for (int w = 0; w < 7; w++) {
                     if (nO[q][w]== 0) {
                         return 0;
@@ -353,17 +366,32 @@ public class GUI implements ActionListener {
                     }
                 }
             }
-            return 3;
+        /**
+         * returns 3 if its a tie.
+         * @return
+         */
+        return 3;
             //Oavgjort
         }
-     
-    
 
 
+    /**
+     * Starts the program.
+     * @param args
+     */
     public static void main(String[] args) {
-        createGUI();
+        /**
+         * @param IOException , try and catch.
+         */
+        try {
+            createGUI();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+
 
 
     @Override
